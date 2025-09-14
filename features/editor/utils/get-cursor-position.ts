e00e8912f7eba7a48da89038a71049d7850af882
Hyunseok Byun <gumyoincirno@gmail.com>
2025-09-14 09:25:59 +0900

    Apply prettier

--- features/editor/utils/get-cursor-position.ts
+++ features/editor/utils/get-cursor-position.ts
@@ -7,14 +7,10 @@ export const getCursorPosition = (editorElement: HTMLDivElement): number => {
     const range = selection.getRangeAt(0)
     let position = 0
 
-    const walker = document.createTreeWalker(
-        editorElement,
-        NodeFilter.SHOW_TEXT,
-        null
-    )
+    const walker = document.createTreeWalker(editorElement, NodeFilter.SHOW_TEXT, null)
 
     let node: Node | null
-    while (node = walker.nextNode()) {
+    while ((node = walker.nextNode())) {
         if (node === range.startContainer) {
             position += range.startOffset
             break
